45b54dbf6aafca6400bafce160d354fadccb616b
hongqiang.zhu <hongqiang.zhu@unity3d.com>
2019-08-15 15:19:11 +0800

    creat new branch

---
+++ New Unity Project/Assets/Scripts/TestDllAndroid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TestDllInAndroid;
+using UnityEngine.UI;
+
+public class TestDllAndroid : MonoBehaviour
+{
+
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestDllInAndroid.Program td = new TestDllInAndroid.Program();
+        int a = td.GetANumber("ssss");
+
+        text.text = a.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Testframe.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Timers;
+using System;
+
+public class Testframe : MonoBehaviour
+{
+
+    float time = 5f;
+    float curtime = 0f;
+    GameObject cube;
+
+    private void Awake()
+    {
+        Color32 c = new Color32();
+        Color color = new Color();
+        Application.targetFrameRate = 30;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = Vector3.zero;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("update");
+
+        curtime += Time.deltaTime;
+        if (curtime > time)
+        {
+            GameObject.Destroy(cube);
+            curtime = 0f;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        //Debug.Log("fixed update");
+    }
+}
